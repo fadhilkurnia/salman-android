de71ecb42891472b89db62b639921a97e35e47df
Fadhil Imam Kurnia <fadhilimamk@gmail.com>
2018-02-24 11:44:43 +0700

    Add butterknife lib, add onclick handler in login

--- app/src/main/java/com/salmanitb/alumnisalman/LoginActivity.java
+++ app/src/main/java/com/salmanitb/alumnisalman/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.salmanitb.alumnisalman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @OnClick(R.id.txt_register)
+    public void gotoRegister() {
+        Intent i = new Intent(this, RegisterActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/salmanitb/alumnisalman/RegisterActivity.java
+++ app/src/main/java/com/salmanitb/alumnisalman/RegisterActivity.java
@@ -1,13 +1,26 @@
 package com.salmanitb.alumnisalman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    @OnClick(R.id.txt_login)
+    public void gotoLogin() {
+        Intent i = new Intent(this, LoginActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
     }
 }
