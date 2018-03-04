6f43c1d44d2e9c01590e87efb0d65d38fd057485
13513060 <13513060@std.stei.itb.ac.id>
2018-03-05 00:43:31 +0700

    add controller navigation to MainActivity

--- app/src/main/java/com/salmanitb/alumnisalman/MainActivity.java
+++ app/src/main/java/com/salmanitb/alumnisalman/MainActivity.java
@@ -1,13 +1,56 @@
 package com.salmanitb.alumnisalman;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle(R.string.title_home);
+                    return true;
+                case R.id.navigation_profile:
+                    toolbar.setTitle(R.string.title_profile);
+                    return true;
+                case R.id.navigation_map:
+                    toolbar.setTitle(R.string.title_map);
+                    return true;
+                case R.id.navigation_gift:
+                    toolbar.setTitle(R.string.title_gift);
+                    return true;
+                case R.id.navigation_contact:
+                    toolbar.setTitle(R.string.title_contact);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_home);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
