6d5bac9631b2fdc02900d0329df567757867dc07
13513060 <13513060@std.stei.itb.ac.id>
2018-03-05 00:56:33 +0700

    add SalmanMenyapaFragment

---
+++ app/src/main/java/com/salmanitb/alumnisalman/SalmanMenyapaFragment.java
@@ -0,0 +1,27 @@
+package com.salmanitb.alumnisalman;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SalmanMenyapaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_salman_menyapa, container, false);
+        return rootView;
+    }
+}
