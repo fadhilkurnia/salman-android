05bb41fc4f42817ade5f791b0ff1b6f40876caed
fadhilimamk <fadhilimamk@gmail.com>
2018-04-12 01:35:38 +0700

    Add input personal in registration

--- app/src/main/java/com/salmanitb/alumnisalman/activity/RegistrationActivity.java
+++ app/src/main/java/com/salmanitb/alumnisalman/activity/RegistrationActivity.java
@@ -1,18 +1,38 @@
 package com.salmanitb.alumnisalman.activity;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.salmanitb.alumnisalman.R;
+import com.salmanitb.alumnisalman.RegistrationPersonalFragment;
 
 import butterknife.ButterKnife;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    Fragment firstStep;
+    Fragment secondStep;
+    Fragment thirdStep;
+    Fragment fourthStep;
+    Fragment fifthStep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         ButterKnife.bind(this);
+
+        firstStep = new RegistrationPersonalFragment();
+
+        loadFragment(firstStep);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.registration_frame, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
