1dcf8bb27eb565460149fb4391ec62a5ec151487
fadhilimamk <fadhilimamk@gmail.com>
2018-04-12 02:56:10 +0700

    Add next button in registration activity

--- app/src/main/java/com/salmanitb/alumnisalman/activity/RegistrationActivity.java
+++ app/src/main/java/com/salmanitb/alumnisalman/activity/RegistrationActivity.java
@@ -1,22 +1,28 @@
 package com.salmanitb.alumnisalman.activity;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.salmanitb.alumnisalman.R;
+import com.salmanitb.alumnisalman.fragment.RegistrationActivityFragment;
+import com.salmanitb.alumnisalman.fragment.RegistrationAlmamaterFragment;
 import com.salmanitb.alumnisalman.fragment.RegistrationPersonalFragment;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    Fragment firstStep;
-    Fragment secondStep;
-    Fragment thirdStep;
-    Fragment fourthStep;
-    Fragment fifthStep;
+    static Fragment firstStep;
+    static Fragment secondStep;
+    static Fragment thirdStep;
+    static Fragment fourthStep;
+    static Fragment fifthStep;
+    Fragment[] stepFragments = {firstStep, secondStep, thirdStep, fourthStep, fifthStep};
+    int stepId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +31,33 @@ public class RegistrationActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         firstStep = new RegistrationPersonalFragment();
+        secondStep = new RegistrationAlmamaterFragment();
+        thirdStep = new RegistrationActivityFragment();
+        fourthStep = new RegistrationActivityFragment();
+        fifthStep = new RegistrationActivityFragment();
 
-        loadFragment(firstStep);
+        loadStepFragment(stepFragments[stepId]);
     }
 
-    private void loadFragment(Fragment fragment) {
+    public void loadStepFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.registration_frame, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @OnClick(R.id.btn_next)
+    protected void gotoNextStep() {
+        if (stepId < stepFragments.length -1) {
+            stepId++;
+            loadStepFragment(stepFragments[stepId]);
+            return;
+        }
+
+        Intent i = new Intent(this, ConfirmActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+
     }
 
     @Override
