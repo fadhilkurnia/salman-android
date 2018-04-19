1385adda696377f2f70017d09de5db4e975c247d
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2018-04-19 15:55:23 +0700

    change edit text in map fragment

--- app/src/main/java/com/salmanitb/alumnisalman/fragment/MapFragment.java
+++ app/src/main/java/com/salmanitb/alumnisalman/fragment/MapFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
@@ -26,12 +27,19 @@ import com.salmanitb.alumnisalman.R;
 import com.salmanitb.alumnisalman.activity.SearchActivity;
 import com.salmanitb.alumnisalman.model.City;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 import static com.salmanitb.alumnisalman.activity.MainActivity.cities;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MapFragment extends Fragment {
+
+
+    @BindView(R.id.input_search)
+    EditText inputSearch;
 
     private GoogleMap googleMap;
 
@@ -44,28 +52,39 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        ButterKnife.bind(this, rootView);
 
-        Spinner city_spinner = (Spinner) rootView.findViewById(R.id.city_spinner);
-
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
-                R.array.cities_array, android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        city_spinner.setAdapter(adapter);
+        inputSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadSearchActivity();
+            }
+        });
+//        Spinner city_spinner = (Spinner) rootView.findViewById(R.id.city_spinner);
+//
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
+//                R.array.cities_array, android.R.layout.simple_spinner_item);
+//
+//        // Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner
+//        city_spinner.setAdapter(adapter);
 
         ImageButton search = (ImageButton) rootView.findViewById(R.id.search_alumni);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), SearchActivity.class);
-                startActivity(intent);
+                loadSearchActivity();
             }
         });
 
         return rootView;
+    }
+
+    private void loadSearchActivity() {
+        Intent intent = new Intent(this.getContext(), SearchActivity.class);
+        startActivity(intent);
     }
 
     @Override
